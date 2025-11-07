17332eb51b11eaa371c71d8605d589397faac9ed
Naledi Mokoena <nm353292@gmail.com>
2025-11-07 10:28:51 +0200

    Clean up URL imports and confirm add/edit/delete book paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register
-from django.urls import path
-from .views import admin_view, librarian_view, member_view
-from django.urls import path
 from . import views
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register,
+    admin_view,
+    librarian_view,
+    member_view,
+    add_book,
+    edit_book,
+    delete_book,
+)
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -17,9 +22,9 @@ urlpatterns = [
     path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
     path('member-role/', member_view, name='member_view'),
-    path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/', views.edit_book, name='edit_book'),
-    path('delete-book/', views.delete_book, name='delete_book'),
+    path('add-book/', add_book, name='add_book'),
+    path('edit-book/', edit_book, name='edit_book'),
+    path('delete-book/', delete_book, name='delete_book'),
 ]
 
 
