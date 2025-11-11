f07bd29d86f3d0802de8430b400233c09fcda75a
Naledi Mokoena <nm353292@gmail.com>
2025-11-11 22:04:17 +0200

    Removed old folders for clean repo reset

--- advanced_features_and_security/config/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Show these columns
-    list_filter = ('publication_year', 'author')            # Add filters
-    search_fields = ('title', 'author')                     # Enable search
-
-admin.site.register(Book, BookAdmin)
