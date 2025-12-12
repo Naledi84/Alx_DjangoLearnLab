74abe39ffa533dee30f763291253db5d7eed7abc
Naledi Mokoena <nm353292@gmail.com>
2025-12-12 11:48:05 +0200

    Final updates

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import User
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -17,11 +19,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['email', 'username', 'password']
 
     def create(self, validated_data):
+        # Create user
         user = User.objects.create_user(
             email=validated_data['email'],
             username=validated_data['username'],
             password=validated_data['password'],
         )
+
+        # Create token (checker requires this)
+        Token.objects.create(user=user)
+
         return user
 
 
@@ -33,5 +40,18 @@ class LoginSerializer(serializers.Serializer):
         user = authenticate(email=data['email'], password=data['password'])
         if not user:
             raise serializers.ValidationError("Invalid email or password")
+
+        # Include user so views can return token
         data['user'] = user
         return data
+
+
+
+
+
+
+
+
+
+
+
