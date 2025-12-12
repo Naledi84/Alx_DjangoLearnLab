15754350f9c4255ef1c0f5966be6b0e540c8414c
Naledi Mokoena <nm353292@gmail.com>
2025-12-12 12:35:26 +0200

    Final updates

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 
     # local apps
     'accounts',
+    'posts',
+    'rest_framework',
 ]
 
 
@@ -137,6 +139,12 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
